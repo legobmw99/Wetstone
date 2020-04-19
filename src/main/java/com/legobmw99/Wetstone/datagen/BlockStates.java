e50b733ee94eede7cbb01252807345220d8e5056
legobmw99 <legobmw99@gmail.com>
2020-04-19 16:34:49 -0400

    New blocks

--- src/main/java/com/legobmw99/Wetstone/datagen/BlockStates.java
+++ src/main/java/com/legobmw99/Wetstone/datagen/BlockStates.java
@@ -2,9 +2,13 @@ package com.legobmw99.Wetstone.datagen;
 
 import com.legobmw99.Wetstone.Wetstone;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.model.Model;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.client.model.generators.ModelFile;
+import org.antlr.v4.runtime.LexerNoViableAltException;
 
 public class BlockStates extends BlockStateProvider {
 
@@ -14,17 +18,20 @@ public class BlockStates extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        singleTextureBlock(Wetstone.STONE_BRICKS.get());
-        singleTextureBlock(Wetstone.NETHER_BRICKS.get());
-        singleTextureBlock(Wetstone.SANDSTONE.get());
+        simpleBlock(Wetstone.STONE_BRICKS.get());
+        simpleBlock(Wetstone.NETHER_BRICKS.get());
+        simpleBottomTopBlock(Wetstone.SANDSTONE.get());
     }
 
-
-    private void singleTextureBlock(Block block) {
-        Wetstone.LOGGER.debug("Creating Block Data for " + block.getRegistryName());
-        simpleBlock(block);
+    private void simpleBottomTopBlock(Block b){
+        ResourceLocation t = blockTexture(b);
+        ModelFile m =  models().cubeBottomTop(t.getPath(), t, extend(t, "_bottom"), extend(t, "_top"));
+        simpleBlock(b, m);
     }
 
+    private ResourceLocation extend(ResourceLocation rl, String suffix) {
+        return new ResourceLocation(rl.getNamespace(), rl.getPath() + suffix);
+    }
 
     @Override
     public String getName() {
